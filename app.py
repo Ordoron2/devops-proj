de52c97998d67aee53b933bfbba34ce04887816b
job <ordoron30@gmail.com>
2025-12-24 10:51:42 +0000

    changed text

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Hello! The DevOps Pipeline is working!", "status": "success"})
+    return jsonify({"message": "Hello! The pipeline is working!", "status": "success"})
 
 @app.route('/health')
 def health():
